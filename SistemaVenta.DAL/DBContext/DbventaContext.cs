b29808bcb1d811c1a080f3711fce7fdbe3de25f9
Jeim05 <lopezvegajeimmy@gmail.com>
2024-09-20 21:43:05 -0600

    Creación de la cadena de conexion

--- SistemaVenta.DAL/DBContext/DbventaContext.cs
+++ SistemaVenta.DAL/DBContext/DbventaContext.cs
@@ -40,9 +40,7 @@ public partial class DbventaContext : DbContext
 
     public virtual DbSet<Venta> Venta { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-2M1PFTDM\\SQLEXPRESS;DataBase=DBVENTA;Integrated Security=true;Encrypt=False");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
