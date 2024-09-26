2037bffdb620bcc7e11c47fd2f8b96c7f9ada244
Jeim05 <lopezvegajeimmy@gmail.com>
2024-09-25 22:25:42 -0600

    Creación de Interfaz e implemntación de la misma

---
+++ SistemaVenta.DAL/Implementacion/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.DAL.DBContext;
+using SistemaVenta.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Implementacion
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        public Task<TEntity> Obtener(Expression<Func<TEntity, bool>> filtro)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<TEntity> Crear(TEntity entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Editar(TEntity entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(TEntity entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<TEntity>> Consulta(Expression<Func<TEntity, bool>> filtro = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SistemaVenta.DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class // decimo que ese TEntity es una clase
+    {
+        Task<TEntity> Obtener(Expression<Func<TEntity, bool>> filtro);
+        Task<TEntity> Crear(TEntity entidad);
+        Task<bool> Editar(TEntity entidad);
+        Task<bool> Eliminar(TEntity entidad);
+        Task<IQueryable<TEntity>> Consulta(Expression<Func<TEntity, bool>> filtro=null);
+
+    }
+}
