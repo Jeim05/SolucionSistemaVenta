9fa376f8273ad323bcbf0bb651613b675480ee82
Jeim05 <lopezvegajeimmy@gmail.com>
2024-09-26 21:27:43 -0600

    Repositorio generico para las funciones crud

--- SistemaVenta.DAL/Implementacion/GenericRepository.cs
+++ SistemaVenta.DAL/Implementacion/GenericRepository.cs
@@ -13,30 +13,78 @@ namespace SistemaVenta.DAL.Implementacion
 {
     public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
     {
-        public Task<TEntity> Obtener(Expression<Func<TEntity, bool>> filtro)
+        //Variable para hacer refrencia a la base de datos
+        private readonly DbventaContext _dbContext;
+
+        // Contructor
+        public GenericRepository(DbventaContext dbContext)
         {
-            throw new NotImplementedException();
+            _dbContext = dbContext;
+        }
+
+        public async Task<TEntity> Obtener(Expression<Func<TEntity, bool>> filtro)
+        {
+            try
+            {
+                TEntity entidad = await _dbContext.Set<TEntity>().FirstOrDefaultAsync(filtro);
+                return entidad;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
 
-        public Task<TEntity> Crear(TEntity entidad)
+        public async Task<TEntity> Crear(TEntity entidad)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _dbContext.Set<TEntity>().Add(entidad);
+                await _dbContext.SaveChangesAsync();
+                return entidad;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<bool> Editar(TEntity entidad)
+        public async Task<bool> Editar(TEntity entidad)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _dbContext.Update(entidad);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<bool> Eliminar(TEntity entidad)
+        public async Task<bool> Eliminar(TEntity entidad)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _dbContext.Remove(entidad);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public Task<IQueryable<TEntity>> Consulta(Expression<Func<TEntity, bool>> filtro = null)
+        public async Task<IQueryable<TEntity>> Consultar(Expression<Func<TEntity, bool>> filtro = null)
         {
-            throw new NotImplementedException();
+            // Se crea una entidad generica
+            // se valida si el filtro es igual a nulo
+            // si el filtro es != null se hace la consulta
+            IQueryable<TEntity> queryEntidad = filtro == null ? _dbContext.Set<TEntity>() : _dbContext.Set<TEntity>().Where(filtro);
+            return queryEntidad;
         }
     }
 }
