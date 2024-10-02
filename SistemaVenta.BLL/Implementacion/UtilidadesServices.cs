8034694b9987a60feb37c52b6f3bd4b8dde6fc45
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-01 21:18:23 -0600

    Creacion de la interfaz y clase para las utilidades.

---
+++ SistemaVenta.BLL/Implementacion/UtilidadesServices.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.BLL.Interfaces;
+using System.Security.Cryptography;
+
+namespace SistemaVenta.BLL.Implementacion
+{
+    public class UtilidadesServices : IUtilidadesService
+    {
+        public string GenerarClave()
+        {
+            string clave = Guid.NewGuid().ToString("N").Substring(0,6); //Brinda un texto aleatorio con numeros y letras entre el 0 y 6
+            return clave;
+        }
+
+        public string ConvertirSha256(string texto)
+        {
+            
+        }
+
+        
+    }
+}
