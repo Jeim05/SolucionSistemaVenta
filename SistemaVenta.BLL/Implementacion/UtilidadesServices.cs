d5859b9fadc4502b2bfc534d6cbdee751266314c
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-01 21:22:35 -0600

    Funtion to encrypt the password

--- SistemaVenta.BLL/Implementacion/UtilidadesServices.cs
+++ SistemaVenta.BLL/Implementacion/UtilidadesServices.cs
@@ -19,7 +19,20 @@ namespace SistemaVenta.BLL.Implementacion
 
         public string ConvertirSha256(string texto)
         {
-            
+            StringBuilder sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256Managed.Create()) { 
+                Encoding enc = Encoding.UTF8;
+
+                byte[] result = hash.ComputeHash(enc.GetBytes(texto));
+
+                foreach (byte b in result)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+            }
+
+            return sb.ToString();
         }
 
         
