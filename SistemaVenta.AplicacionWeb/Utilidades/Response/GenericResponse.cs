1585484db00f1558a177c882c088ca55629f66a3
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-01 21:52:21 -0600

    Utilities for responses in the website

---
+++ SistemaVenta.AplicacionWeb/Utilidades/Response/GenericResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaVenta.AplicacionWeb.Utilidades.Response
+{
+    // Esta clase la utilizamos como respuesta a todas las solicitudes que se hagan al sitio web
+    public class GenericResponse<TObject>
+    {
+        public bool Estado { get; set; }
+        public string? Mensaje { get; set; }
+        public TObject? Objeto { get; set; }
+        public List<TObject>? ListaObjeto { get; set; }
+    }
+}
