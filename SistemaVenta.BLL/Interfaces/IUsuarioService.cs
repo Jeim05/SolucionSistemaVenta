871157a91e71f2ad56fbb9dca208aa5eb7ed3a2c
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-03 22:05:58 -0600

    Interfaz para administrar los usuarios

---
+++ SistemaVenta.BLL/Interfaces/IUsuarioService.cs
@@ -0,0 +1,17 @@
+﻿using SistemaVenta.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.BLL.Interfaces
+{
+    public interface IUsuarioService
+    {
+        // En esta clase se incluyen todos los métodos de la clase usuario
+
+        Task<List<Usuario>> Lista();
+        Task<Usuario> Grear(Usuario entidad, Stream foto=null, string NombreFoto="", string UrlPlantilla);
+    }
+}
