bb84e59e721f7bb5690f0311b4943b4fea674bf4
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-08 22:27:33 -0600

    Controller para administrar usuarios

--- SistemaVenta.AplicacionWeb/Controllers/UsuarioController.cs
+++ SistemaVenta.AplicacionWeb/Controllers/UsuarioController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using AutoMapper;
+using Newtonsoft.Json;
+using SistemaVenta.AplicacionWeb.Models.ViewModels;
+using SistemaVenta.AplicacionWeb.Utilidades;
+
 namespace SistemaVenta.AplicacionWeb.Controllers
 {
     public class UsuarioController : Controller
