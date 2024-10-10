8890618b27835aa1a2f1cb69dc2216a7a5912481
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-09 22:31:54 -0600

    Funcionalidad para guardar usuario

--- SistemaVenta.AplicacionWeb/Controllers/UsuarioController.cs
+++ SistemaVenta.AplicacionWeb/Controllers/UsuarioController.cs
@@ -6,6 +6,7 @@ using SistemaVenta.AplicacionWeb.Models.ViewModels;
 using SistemaVenta.AplicacionWeb.Utilidades;
 using SistemaVenta.Entity;
 using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.AplicacionWeb.Utilidades.Response;
 
 namespace SistemaVenta.AplicacionWeb.Controllers
 {
@@ -32,6 +33,43 @@ namespace SistemaVenta.AplicacionWeb.Controllers
         {
             List<VMRol> vmListaRoles = _mapper.Map<List<VMRol>>(await _rolServices.Lista()); // convierte la lista
             return StatusCode(StatusCodes.Status200OK, vmListaRoles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            List<VMUsuario> vmUsuarioLista = _mapper.Map<List<VMUsuario>>(await _usuarioService.Lista()); // convierte la lista
+            return StatusCode(StatusCodes.Status200OK, new {data=vmUsuarioLista});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear([FromForm] IFormFile foto, [FromForm] string modelo) // Indicamos que recibimos parametros desde un form
+        {
+            GenericResponse<VMUsuario> gResponse = new GenericResponse<VMUsuario>();
+            try
+            {
+                VMUsuario vmUsuario = JsonConvert.DeserializeObject<VMUsuario>(modelo);
+
+                string nombreFoto = "";
+                Stream fotoStream = null;
+
+                if (foto != null) { 
+                string nombre_en_codigo = Guid.NewGuid().ToString("N");
+                    string extension = Path.GetExtension(foto.FileName); // se obtiene la extension de la foto
+                    nombreFoto = string.Concat(nombre_en_codigo); // se da un nuevo nombre a la foto
+                    fotoStream = foto.OpenReadStream();
+                }
+                // se crea el acceso a la plantilla de correo
+                string urlPlantillaCorreo = $"{this.Request.Scheme}://{this.Request.Host}/Plantilla/EnviarClave?correo=[correo]&clave=[clave]";
+
+                Usuario usuario_creado = await _usuarioService.Crear(_mapper.Map<Usuario>(vmUsuario), fotoStream, nombreFoto, urlPlantillaCorreo);
+
+                vmUsuario = _mapper.Map<VMUsuario>(usuario_creado);
+            }
+            catch (Exception)
+            {
+
+            }
         }
     }
 }
