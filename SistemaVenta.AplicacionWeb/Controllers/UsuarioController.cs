5798e4f2a5d8939d6fc0c0074c6d3ef3515b3202
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-10 21:32:11 -0600

    Funcionalidades crud en el controller

--- SistemaVenta.AplicacionWeb/Controllers/UsuarioController.cs
+++ SistemaVenta.AplicacionWeb/Controllers/UsuarioController.cs
@@ -45,6 +45,7 @@ namespace SistemaVenta.AplicacionWeb.Controllers
         [HttpPost]
         public async Task<IActionResult> Crear([FromForm] IFormFile foto, [FromForm] string modelo) // Indicamos que recibimos parametros desde un form
         {
+            // Lo usamos como un formato estandar para las respuestas
             GenericResponse<VMUsuario> gResponse = new GenericResponse<VMUsuario>();
             try
             {
@@ -65,11 +66,70 @@ namespace SistemaVenta.AplicacionWeb.Controllers
                 Usuario usuario_creado = await _usuarioService.Crear(_mapper.Map<Usuario>(vmUsuario), fotoStream, nombreFoto, urlPlantillaCorreo);
 
                 vmUsuario = _mapper.Map<VMUsuario>(usuario_creado);
-            }
-            catch (Exception)
-            {
 
+                gResponse.Estado = true;
+                gResponse.Objeto = vmUsuario;
             }
+            catch (Exception ex)
+            {
+                gResponse.Estado = false;
+                gResponse.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, new {data = gResponse});
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Editar([FromForm] IFormFile foto, [FromForm] string modelo) // Indicamos que recibimos parametros desde un form
+        {
+            // Lo usamos como un formato estandar para las respuestas
+            GenericResponse<VMUsuario> gResponse = new GenericResponse<VMUsuario>();
+            try
+            {
+                VMUsuario vmUsuario = JsonConvert.DeserializeObject<VMUsuario>(modelo);
+
+                string nombreFoto = "";
+                Stream fotoStream = null;
+
+                if (foto != null)
+                {
+                    string nombre_en_codigo = Guid.NewGuid().ToString("N");
+                    string extension = Path.GetExtension(foto.FileName); // se obtiene la extension de la foto
+                    nombreFoto = string.Concat(nombre_en_codigo); // se da un nuevo nombre a la foto
+                    fotoStream = foto.OpenReadStream();
+                }
+
+                Usuario usuario_editado = await _usuarioService.Editar(_mapper.Map<Usuario>(vmUsuario), fotoStream, nombreFoto);
+
+                vmUsuario = _mapper.Map<VMUsuario>(usuario_editado);
+
+                gResponse.Estado = true;
+                gResponse.Objeto = vmUsuario;
+            }
+            catch (Exception ex)
+            {
+                gResponse.Estado = false;
+                gResponse.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, new { data = gResponse });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Eliminar(int IdUsuario)
+        {
+            GenericResponse<string> gResponse = new GenericResponse<string>();
+            try
+            {
+                gResponse.Estado = await _usuarioService.Eliminar(IdUsuario);
+            }
+            catch (Exception ex)
+            {
+                gResponse.Estado = false;
+                gResponse.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, new { data = gResponse });
+
         }
     }
+
+    
 }
